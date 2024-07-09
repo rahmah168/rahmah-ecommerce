bf7d1a2783a87812e8a06b4d60c1e9af4d6f8b31
unknown <rahmahsaif6@gmail.com>
2024-07-09 11:23:16 +0400

    home4

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -27,18 +27,13 @@ function LoginPage({ onLogin }) {
       if (response.data && response.data.length > 0) {
         onLogin(response.data);
         navigate("/product");
-        if (user.email === 'admin@gmail.com') {
-          return <navigate to="/admin" />;
-      } else {
-          return <navigate to="/products" />;
-      }
       } else {
         setErrorMessage("Invalid Email or Password");
-        setValidated(false); 
+        setValidated(false); // Ensure the form is marked as invalid
       }
     } catch (error) {
       setErrorMessage("An error occurred. Please try again.");
-      setValidated(false); 
+      setValidated(false); // Ensure the form is marked as invalid
     }
   };
 
